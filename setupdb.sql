9d412b58a106a7279a683d41a2be33fadc117604
liwilson <wilia832@student.otago.ac.nz>
2022-10-06 11:35:33 +1300

    updated sql

--- setupdb.sql
+++ setupdb.sql
@@ -1,6 +1,9 @@
+CREATE DATABASE mydb;
+USE mydb;
+DROP TABLE IF EXISTS noteboard;
 CREATE TABLE noteboard(
     id int not null auto_increment,
     title varchar(100) not null,
     body text not null,
     primary key(id)
-)
+);
