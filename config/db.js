d0d35cebd7ce36fdfdfeca510083daf8c825e676
liwilson <wilia832@student.otago.ac.nz>
2022-10-06 11:35:22 +1300

    added sql for setting up database. Updated package

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
-	//password:/* password goes here */,
+	password:/*Your password here */,
 	database:'mydb'
 });
 connection.connect(function(error){
---
+++ setupdb.sql
@@ -0,0 +1,6 @@
+CREATE TABLE noteboard(
+    id int not null auto_increment,
+    title varchar(100) not null,
+    body text not null,
+    primary key(id)
+)
